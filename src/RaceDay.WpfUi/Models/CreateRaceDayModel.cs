3ea08ae348e553d142a7b5192f901d4bfc445667
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2023-05-01 00:19:01 +0100

    Improve New Race Day dialog style

--- src/RaceDay.WpfUi/Models/CreateRaceDayModel.cs
+++ src/RaceDay.WpfUi/Models/CreateRaceDayModel.cs
@@ -5,31 +5,31 @@ namespace RaceDay.WpfUi.Models;
 
 public class CreateRaceDayModel : ObservableObject
 {
-    private string _name = string.Empty;
-    private float _signUpFee;
-    private float _lapDistance;
-    private float _petrolCostPerLap;
+    private string? _name = string.Empty;
+    private float? _signUpFee;
+    private float? _lapDistance;
+    private float? _petrolCostPerLap;
     public Guid Guid { get; set; }
 
-    public string Name
+    public string? Name
     {
         get => _name;
         set => SetField(ref _name, value);
     }
 
-    public float SignUpFee
+    public float? SignUpFee
     {
         get => _signUpFee;
         set => SetField(ref _signUpFee, value);
     }
 
-    public float LapDistance
+    public float? LapDistance
     {
         get => _lapDistance;
         set => SetField(ref _lapDistance, value);
     }
 
-    public float PetrolCostPerLap
+    public float? PetrolCostPerLap
     {
         get => _petrolCostPerLap;
         set => SetField(ref _petrolCostPerLap, value);
