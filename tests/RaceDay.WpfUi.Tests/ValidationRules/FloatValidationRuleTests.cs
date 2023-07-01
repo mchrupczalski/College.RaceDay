5fc027960b5bdef6e79bfb43162c02a098de0db4
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2023-07-01 18:59:24 +0100

    Refactor FloatValidationRuleTests to use float values for min and max
    The FloatValidationRuleTests class has been refactored to accept float values for the minimum (min) and maximum (max) properties. This change allows for more flexibility in testing different ranges of floating-point numbers. The Validate_ReturnsFalse_WhenStringIsLessThanMin, Validate_ReturnsFalse_WhenStringIsGreaterThanMax, and Validate_ReturnsTrue_WhenStringIsAFloatAndBetweenMinAndMax methods have been updated accordingly to pass the new float values as parameters.

--- tests/RaceDay.WpfUi.Tests/ValidationRules/FloatValidationRuleTests.cs
+++ tests/RaceDay.WpfUi.Tests/ValidationRules/FloatValidationRuleTests.cs
@@ -31,34 +31,34 @@ public class FloatValidationRuleTests
     
     // Test Validate when string is less than Min returns false
     [Theory]
-    [InlineData("0.0")]
-    [InlineData("-1.0")]
-    public void Validate_ReturnsFalse_WhenStringIsLessThanMin(object value)
+    [InlineData(1f, "0.0")]
+    [InlineData(1f, "-1.0")]
+    public void Validate_ReturnsFalse_WhenStringIsLessThanMin(float min, object value)
     {
-        var rule = new FloatValidationRule { Min = 1 };
+        var rule = new FloatValidationRule { Min = min };
         var result = rule.Validate(value, CultureInfo.CurrentCulture);
         Assert.False(result.IsValid);
     }
     
     // Test Validate when string is greater than Max returns false
     [Theory]
-    [InlineData("2.1")]
-    [InlineData("3.0")]
-    public void Validate_ReturnsFalse_WhenStringIsGreaterThanMax(object value)
+    [InlineData(2f,"2.1")]
+    [InlineData(2f, "3.0")]
+    public void Validate_ReturnsFalse_WhenStringIsGreaterThanMax(float max, object value)
     {
-        var rule = new FloatValidationRule { Max = 2 };
+        var rule = new FloatValidationRule { Max = max };
         var result = rule.Validate(value, CultureInfo.CurrentCulture);
         Assert.False(result.IsValid);
     }
     
     // Test Validate when string is a float and between Min and Max returns true
     [Theory]
-    [InlineData("1.0")]
-    [InlineData("1.5")]
-    [InlineData("2.0")]
-    public void Validate_ReturnsTrue_WhenStringIsAFloatAndBetweenMinAndMax(object value)
+    [InlineData(1f, 2f, "1.0")]
+    [InlineData(1f, 2f, "1.5")]
+    [InlineData(1f, 2f, "2.0")]
+    public void Validate_ReturnsTrue_WhenStringIsAFloatAndBetweenMinAndMax(float min, float max, object value)
     {
-        var rule = new FloatValidationRule { Min = 1, Max = 2 };
+        var rule = new FloatValidationRule { Min = min, Max = max };
         var result = rule.Validate(value, CultureInfo.CurrentCulture);
         Assert.True(result.IsValid);
     }
